f615cdbe4eb0e393897a3204bb7e31c7310353b6
shristipathak <shristi.pathak@sirionlabs.com>
2025-08-16 23:01:03 +0530

    project and filename changed

--- src/test/java/com/example/learninglld/DesignPattern.java
+++ src/test/java/com/example/learninglld/DesignPattern.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LearningLldApplicationTests {
+class DesignPattern {
 
     @Test
     void contextLoads() {
